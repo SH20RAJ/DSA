12f6dbfdea717581b2c317cb2333f4fc4b1c61a3
sh20raj <shaswatraj3@gmail.com>
2024-05-27 23:08:21 +0530

    selectionsort v2

--- sortingalso/selectionsort.c
+++ sortingalso/selectionsort.c
@@ -4,7 +4,7 @@
 
 
 int main() {
-    
+
     
     int arr[] = {1,5,2,7,4,9};
     // sorted array = 1,2,4,5,7,9
@@ -12,18 +12,20 @@ int main() {
 
     for(int i = 0; i <= length-1; i++){
         int lowest, position;
-        for (int j = i; j < length -2; j++)
-        {
+        printf(" i = %d \n", i);
+
+        for (int j = i; j < length -2; j++){
+            printf(" j = %d \n", j);
              lowest = arr[j];
              position = j;
-            if(arr[j] > arr[j+1]){
+            if(lowest < arr[j+1]){
                 lowest = arr[j+1];
-                position = j+1;
-            } else {
-                lowest = arr[j];
                 position = j;
-            }
+                printf( " lowest = %d ,  position =  %d \n" , lowest, position);
+            } 
         }
+
+        printf("outof the j loop \n");
         // swap nos
         int temp = arr[i];
         arr[i] = lowest;
@@ -31,20 +33,18 @@ int main() {
     
 }
 
+printf(" outof the i loop \n");
+
+
     // printf("%d", arr[2]);
     // printf("%d", arr[1]);
     // Output = 4,2
 
     // printing the array 
-    for (int i = 0; i < length; i++)
-    {
-        printf(" %d , ", arr[i]);
+    printf(" printing the final array ");
+    for (int i = 0; i < length; i++){
+        printf(" %d  ", arr[i]);
     }
-    
-
-
-
-    
 
 
 
